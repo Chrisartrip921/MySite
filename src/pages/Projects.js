1b7277852c8380f12e00603e5d9296e34d66dbc4
Christopher Artrip <christopherartrip@gmail.com>
2021-05-07 01:54:49 -0400

    Made changes to function, user can only open one project at a time. New button design

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,58 +1,88 @@
-import React, { useState } from 'react'
-import { Container, Row, Col } from 'react-bootstrap';
-import '../index.css';
+import React, { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "../index.css";
 
 export default function Projects() {
-    const [isVisible, setIsVisible] = useState(false);
-    
-    function projectButton(e){
-        e.preventDefault();
-        console.log("Hello");
-        setIsVisible(!isVisible);
+  const [isFindGroupVisible, setIsFindGroupVisible] = useState(false);
+  const [isGiphyVisible, setIsGiphyVisible] = useState(false);
+
+  function findGroupButton(e) {
+    e.preventDefault();
+    setIsFindGroupVisible(!isFindGroupVisible);
+    if(isGiphyVisible){
+        setIsGiphyVisible(false)
     }
-    
-    return (
-        <div>
-            <Container>
-                <Row>
-                    <Col>
-                        <div className="mt-4 mb-3">
-                            <b>Project Name: FindGroup</b> 
-                        </div>
-                        <button className=" mb-3" onClick={projectButton}>
-                            Reveal FindGroup!
-                        </button>
-                        <div>
-                            <div>
-                                {
-                                isVisible 
-                                ? 
-                                    (
-                                        <div className="iframe-wrapper">
-                                            <iframe 
-                                                title="Project Video" 
-                                                src="https://www.youtube.com/embed/uzNrGFSndEs&ab_channel=ChristopherArtrip"
-                                                allowFullScreen
-                                                webkitallowfullscreen="true"
-                                                mozallowfullscreen="true"
-                                                frameBorder="0"
-                                            />
-                                        </div>
-                                    ) 
-                                :
-                                    (
-                                        <div>
+  }
 
-                                        </div>
-                                    ) 
-                                }
-                            </div>
-                        </div>
-                    </Col>
-                </Row>
-                
-            </Container>
-        </div>        
+  function giphyButton(e) {
+    e.preventDefault();
+    setIsGiphyVisible(!isGiphyVisible);
+    if(isFindGroupVisible){
+        setIsFindGroupVisible(false)
+    }
+  }
 
-    )
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col>
+            <div className="mt-4 mb-3">
+              <b>Project Name: FindGroup</b>
+            </div>
+            <button className="button mb-3" onClick={findGroupButton}>
+              Reveal FindGroup!
+            </button>
+            <div>
+              <div>
+                {isFindGroupVisible ? (
+                  <div className="iframe-wrapper">
+                    <iframe
+                      title="Project Video"
+                      src="https://www.youtube.com/embed/uzNrGFSndEs"
+                      allowFullScreen
+                      webkitallowfullscreen="true"
+                      mozallowfullscreen="true"
+                      frameBorder="0"
+                    />
+                  </div>
+                ) : (
+                  <div></div>
+                )}
+              </div>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className="mt-4 mb-3">
+              <b>Project Name: Giphy-App</b>
+            </div>
+            <button className="button mb-3" onClick={giphyButton}>
+              Reveal Giphy-App!
+            </button>
+            <div>
+              <div>
+                {isGiphyVisible ? (
+                  
+                  <div className="iframe-wrapper">
+                    <iframe
+                      title="Project Video"
+                      src="https://www.youtube.com/embed/kI8dsnqMt-U"
+                      allowFullScreen
+                      webkitallowfullscreen="true"
+                      mozallowfullscreen="true"
+                      frameBorder="0"
+                    />
+                  </div>
+                ) : (
+                  <div></div>
+                )}
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
